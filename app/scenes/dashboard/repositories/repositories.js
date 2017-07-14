d4dc9de27d3e7f734855537a5b533554f07ff8d5
moiseshilario <moiseshilariu@gmail.com>
2017-07-14 15:49:28 -0300

    Implement Repositories Component

--- app/scenes/dashboard/repositories/repositories.js
+++ app/scenes/dashboard/repositories/repositories.js
@@ -1,4 +1,4 @@
-import { Badge, Separator } from '../../components'
+import { Badge, Separator } from '../../../components'
 import React, { Component } from 'react'
 import { styles } from './repositories.css'
 
@@ -9,14 +9,38 @@ import {
   TouchableHighlight
 } from 'react-native'
 
-class Repositories extends Component {
-  render(){
-    return (
-      <ScrollView style={styles.container}>
-        <Badge userInfo={this.props.userInfo}/>
-      </ScrollView>
-    )
+const Repositories = ({ userInfo, repos }) => {
+  openPage = (url) => {
+    console.log('the url: ', url)
   }
+
+  const list = repos.map((item, index) => {
+    const desc = repos[index].description ? <Text style={styles.description}> {repos[index].description} </Text> : <View/>
+
+    return (
+      <View key={index}>
+        <View style={styles.rowContainer}>
+          <TouchableHighlight
+            onPress={this.openPage(repos[index.html_url])}
+            underlayColor='transparent'>
+            <Text style={styles.name}> {repos[index].name} </Text>
+          </TouchableHighlight>
+          <Text style={styles.stars}> Stars: {repos[index].stargazers_count}</Text>
+          {desc}
+        </View>
+        <Separator />
+      </View>
+    )
+  })
+
+  return (
+    <ScrollView style={styles.container}>
+      <Badge userInfo={userInfo} />
+      {list}
+    </ScrollView>
+  )
 }
+
+
 
 export default Repositories
