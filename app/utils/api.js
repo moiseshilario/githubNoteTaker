26d2f06bb85ac5845f8a7a5e2c94fe6c8fbcca55
moiseshilario <moiseshilariu@gmail.com>
2017-07-17 14:53:35 -0300

    Add getNotes and addNote functions in the api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -8,6 +8,19 @@ const API = {
     username = username.toLowerCase().trim();
     let url = `https://api.github.com/users/${username}/repos`
     return fetch(url).then((res) => res.json())
+  },
+  getNotes: (username) => {
+    username = username.toLowerCase().trim()
+    let url = `https://github-saver-61205.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json())
+  },
+  addNote: (username, note) => {
+    username = username.toLowerCase().trim()
+    let url = `https://github-saver-61205.firebaseio.com/ ${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json())
   }
 }
 
