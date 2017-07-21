8ca3d52900ec8c443e4f4f079cf4b483e7aaaee6
moiseshilario <moiseshilariu@gmail.com>
2017-07-21 15:34:32 -0300

    Add updateNote API function

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,34 +1,41 @@
 const API = {
   getBio: (username) => {
-    username = username.toLowerCase().trim();
+    username = username.toLowerCase().trim()
     let url = `https://api.github.com/users/${username}`
     return fetch(url).then((res) => res.json())
   },
   getRepos: (username) => {
-    username = username.toLowerCase().trim();
+    username = username.toLowerCase().trim()
     let url = `https://api.github.com/users/${username}/repos`
     return fetch(url).then((res) => res.json())
   },
   getNotes: (username) => {
     username = username.toLowerCase().trim()
-    let url = `https://github-saver-61205.firebaseio.com/${username}.json`;
+    let url = `https://github-saver-61205.firebaseio.com/${username}.json`
     return fetch(url).then((res) => res.json())
   },
   addNote: (username, note) => {
     username = username.toLowerCase().trim()
-    let url = `https://github-saver-61205.firebaseio.com/${username}.json`;
+    let url = `https://github-saver-61205.firebaseio.com/${username}.json`
     return fetch(url, {
       method: 'post',
       body: JSON.stringify(note)
     }).then((res) => res.json())
   },
   deleteNote: (username, noteKey) => {
-    console.log(noteKey)
     username = username.toLowerCase().trim()
-    let url = `https://github-saver-61205.firebaseio.com/${username}/${noteKey}.json`;
+    let url = `https://github-saver-61205.firebaseio.com/${username}/${noteKey}.json`
     return fetch(url, {
       method: 'delete'
     }).then((res) => res.json())
+  },
+  updateNote: (username, noteKey, noteText) => {
+    username = username.toLowerCase().trim()
+    let url = `https://github-saver-61205.firebaseio.com/${username}/${noteKey}.json`
+    return fetch(url, {
+      method: 'put',
+      body: JSON.stringify(noteText)
+    })
   }
 }
 
